c1c622cda19369bbf11fe33a7c224a062e6917ad
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-02-08 12:05:45 -0500

    Added unit test for cuisine search

--- app/src/test/java/com/cookit/ExampleUnitTest.kt
+++ app/src/test/java/com/cookit/ExampleUnitTest.kt
@@ -1,8 +1,10 @@
 package com.cookit
 
+import junit.framework.Assert.*
+import kotlinx.coroutines.test.runTest
+import org.junit.Rule
 import org.junit.Test
-
-import org.junit.Assert.*
+import org.junit.rules.TestRule
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +12,45 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
+
+    @get:Rule
+    var rule: TestRule = InstantTaskExecutorRule()
+
+    lateinit var recipeService : RecipeService
+    var allRecipes : Set<Recipe>? = HashSet<Recipe>()
+
+
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+
+    @Test
+    fun `Given recipe data is available When I search for Japanese Then I should receive Tonkotsu Ramen` () = runTest {
+        givenRecipeServiceIsInitialized()
+        whenRecipeDataIsParsed()
+        thenRecipeCollectionShouldContainTonkotsuRamen()
+    }
+
+    private fun givenRecipeServiceIsInitialized() {
+        recipeService = RecipeService
+    }
+
+    private suspend fun whenRecipeDataIsParsed() {
+        allRecipes = recipeService.getPlants()
+    }
+
+    private fun thenRecipeCollectionShouldContainTonkotsuRamen() {
+        assertNotNull(allRecipes)
+        assertTrue(allRecipes!!.isNotEmpty())
+        var containsTonkotsuRamen = false
+        allRecipes!!.forEach {
+            if (it.cuisine.equals(("Japanese")) && it.name.equals("Tonkotsu Ramen")) {
+                containsTonkotsuRamen = true
+            }
+        }
+        assertTrue(containsTonkotsuRamen)
+    }
+
+
 }
