c23c1dbb405147fb0455b494316f30e08f3b7c2d
Zilong Chen <chen3zl@mail.uc.edu>
2022-02-22 17:07:39 -0500

    Add recipe: Hongshao Pork

--- app/src/main/java/com/cookit/service/RecipeServiceStub.kt
+++ app/src/main/java/com/cookit/service/RecipeServiceStub.kt
@@ -50,6 +50,13 @@ class RecipeServiceStub {
             "English",
             mutableMapOf("Bananas" to "1 cup", "Pecans" to "1 tbsp", "flour" to "1 cup")
         ))
+        fakeRecipe.add(Recipe(
+            "Hongshao Pork",
+            "Wash the pork, marinate it and put it into a pot with cold water. Add seasoning and soy sauce, boil the water over high heat, and then cook over medium low heat for half an hour.",
+            "Dinner",
+            "Chinese",
+            mutableMapOf("Pork" to "1 lb", "soy sauce" to "2 spoons", "oyster sauce" to "1 spoons", "green onion" to "1 oz","ginger" to "1 oz", "anise" to "1","Chinese prickly ash" to "7","sugar" to "12 g")
+        ))
         return fakeRecipe
     }
 
