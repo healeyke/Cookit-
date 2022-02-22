e64cfe3d5dab1a48245eeb745bd5a45a32a13ad3
Zilong Chen <chen3zl@mail.uc.edu>
2022-02-22 18:35:09 -0500

    Change unit test

--- app/src/test/java/com/cookit/RecipeServiceIntegrationTest.kt
+++ app/src/test/java/com/cookit/RecipeServiceIntegrationTest.kt
@@ -66,6 +66,12 @@ class RecipeServiceIntegrationTest {
         thenRecipeCollectionShouldContainHongShaoPork()
     }
 
+    @Test
+    fun `Given recipe data is available When I search for Chinese Then I should receive Fried Yum` () = runTest {
+        givenRecipeServiceIsInitialized()
+        whenRecipeDataIsParsed()
+        thenRecipeCollectionShouldContainFriedYum()
+    }
 
 
 
@@ -146,6 +152,17 @@ class RecipeServiceIntegrationTest {
             }
         }
         assertTrue(containsHongShaoPork)
+    }
+    private fun thenRecipeCollectionShouldContainFriedYum() {
+        assertNotNull(allRecipes)
+        assertTrue(allRecipes!!.isNotEmpty())
+        var containsFriedYum = false
+        allRecipes!!.forEach {
+            if (it.cuisine.equals(("Chinese")) && it.name.equals("Fried Yum")) {
+                containsFriedYum = true
+            }
+        }
+        assertTrue(containsFriedYum)
     }
 
 
