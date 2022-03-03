97e901699e201f3d7edafe3b8aad35baa45dff6f
schottlw <schottlw@mail.uc.edu>
2022-03-03 11:13:10 -0500

    Added comments to RetrofitClientInstance to clarify work for code review

--- app/src/main/java/com/cookit/RetrofitClientInstance.kt
+++ app/src/main/java/com/cookit/RetrofitClientInstance.kt
@@ -6,12 +6,11 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitClientInstance {
     private var retrofit: Retrofit? = null
     private val BASE_URL = ""
+    // data used for testing located in RecipeServiceStub class
 
     val retrofitInstance : Retrofit?
         get() {
-            // has this object been created yet?
             if (retrofit == null) {
-                // create it
                 retrofit = retrofit2.Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
