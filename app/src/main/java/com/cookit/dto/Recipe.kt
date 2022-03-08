eae9026e7b53138509d536293e6534a4bdf8e2f8
MrPandey2k <66745368+MrPandey2k@users.noreply.github.com>
2022-03-08 16:02:01 -0500

    Changes to recipe object

--- app/src/main/java/com/cookit/dto/Recipe.kt
+++ app/src/main/java/com/cookit/dto/Recipe.kt
@@ -1,11 +1,16 @@
 package com.cookit.dto
 
+import java.util.*
+import kotlin.collections.LinkedHashMap
+
 data class Recipe(
     var name : String,
-    var instructions : Array<String>,
+    var instructions : Array<String> = emptyArray(),
     var category : String = "",
     var cuisine : String = "",
-    var ingredients : MutableMap<String, String> = LinkedHashMap<String, String>()
+    var ingredients : MutableMap<String, String> = LinkedHashMap<String, String>(),
+    var id : String ="",
+    var tags : Array<String> = emptyArray()
     ) {
     override fun toString(): String {
         return name
