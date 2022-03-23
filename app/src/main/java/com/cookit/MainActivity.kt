c7266faf665aabb6ddecf08d6f802a9ff2d45986
schottlw <schottlw@mail.uc.edu>
2022-03-23 15:17:30 -0400

    Added Recipe Name text field for 'search' part of app

--- app/src/main/java/com/cookit/MainActivity.kt
+++ app/src/main/java/com/cookit/MainActivity.kt
@@ -3,16 +3,11 @@ package com.cookit
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.cookit.ui.theme.CookitTheme
-
-// For sprint 1, we mostly focused on testing with classes and services
-// Please see other files for code review such as dao, dto, service packages & integration test class, or check our commits for what we have done so far
-// UI coming soon!
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +16,7 @@ class MainActivity : ComponentActivity() {
             CookitTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    RecipeFields("Android")
                 }
             }
         }
@@ -29,14 +24,20 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun RecipeFields(name: String) {
+    var recipeName by remember { mutableStateOf("")}
+
+    OutlinedTextField(
+        value = recipeName,
+        onValueChange = {recipeName = it},
+        label = { Text(stringResource(R.string.recipeName))}
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CookitTheme {
-        Greeting("Android")
+        RecipeFields("Android")
     }
 }
