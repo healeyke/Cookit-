84e7959da81ed1ce5ceb7354b479d234540b29be
schottlw <schottlw@mail.uc.edu>
2022-03-23 19:04:49 -0400

    Added style modifiers to elements

--- app/src/main/java/com/cookit/MainActivity.kt
+++ app/src/main/java/com/cookit/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.cookit
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.cookit.ui.theme.CookitTheme
 
 class MainActivity : ComponentActivity() {
@@ -35,23 +38,36 @@ fun RecipeFields(name: String) {
         OutlinedTextField(
             value = recipeName,
             onValueChange = {recipeName = it},
-            label = { Text(stringResource(R.string.recipeName))}
+            label = { Text(stringResource(R.string.recipeName))},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp)
         )
         OutlinedTextField(
             value = category,
             onValueChange = {category = it},
-            label = { Text(text = stringResource(R.string.category))}
+            label = { Text(text = stringResource(R.string.category))},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp)
         )
         OutlinedTextField(
             value = cuisine,
             onValueChange = {cuisine = it},
-            label = { Text(text = stringResource(R.string.cuisine))}
+            label = { Text(text = stringResource(R.string.cuisine))},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(start = 10.dp, end = 10.dp)
         )
         Button (
+            modifier = Modifier
+                .padding(10.dp),
             onClick = { /*TODO*/ }
-        ){
+        )
+        {
             Text(text = "Search")
         }
+
     }
 }
 
