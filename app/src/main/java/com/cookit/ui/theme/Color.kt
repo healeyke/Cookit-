8d9671b82a04694af32bdadcb3aeff43a2dada44
healeyke <healeyke@mail.uc.edu>
2022-03-24 10:05:10 -0400

    adjusted Green400 to work on light mode

--- app/src/main/java/com/cookit/ui/theme/Color.kt
+++ app/src/main/java/com/cookit/ui/theme/Color.kt
@@ -3,6 +3,6 @@ package com.cookit.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val Green200 = Color(0xFFcfe1b9)
-val Green400 = Color(0xFF79a739)
+val Green400 = Color(0xFF56803B)
 val Green600 = Color(0xFF3F5E2B)
 val Orange400 = Color(0xFFDF7A44)
