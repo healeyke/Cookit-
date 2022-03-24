f3ee7477a9e417da1d40498812ddef7201b02266
healeyke <healeyke@mail.uc.edu>
2022-03-24 09:56:39 -0400

    added color scheme to theme and color files

--- app/src/main/java/com/cookit/ui/theme/Theme.kt
+++ app/src/main/java/com/cookit/ui/theme/Theme.kt
@@ -7,15 +7,15 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Green200,
+    primaryVariant = Green600,
+    secondary = Orange400
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = Green400,
+    primaryVariant = Green600,
+    secondary = Orange400
 
     /* Other default colors to override
     background = Color.White,
