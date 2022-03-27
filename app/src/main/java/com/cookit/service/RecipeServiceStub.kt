5efb1ef2a33350c0731fbce88425d0d7401650a5
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-03-27 15:22:23 -0400

    fix RecipeServiceStub.kt again

--- app/src/main/java/com/cookit/service/RecipeServiceStub.kt
+++ app/src/main/java/com/cookit/service/RecipeServiceStub.kt
@@ -22,63 +22,63 @@ internal class RecipeServiceStub {
             Recipe(
                 "001",
                 "Hot Pot",
-                "Boil water",
                 "Dinner",
                 "Chinese",
+                "Boil water",
                 "",
                 mutableMapOf("Water" to "10 cups", "Spices" to "5 grams", "Beef" to "6 oz")
             ),
             Recipe(
                 "002",
                 "PBJ",
-                "Get bread, put jelly and peanut butter on bread",
                 "Vegan",
                 "American",
+                "Get bread, put jelly and peanut butter on bread",
                 "",
                 mutableMapOf("Bread" to "2 slices", "Jelly" to "1 tsp", "Peanut Butter" to "2 tsp")
             ),
             Recipe(
                 "003",
                 "Chicken Alfredo",
-                "Cook chicken, boil noodles, add alfredo sauce",
                 "Dinner",
                 "Italian",
+                "Cook chicken, boil noodles, add alfredo sauce",
                 "",
                 mutableMapOf("Chicken" to "6 oz", "Noodles" to "6 oz", "Sauce" to "6 oz")
             ),
             Recipe(
                 "004",
                 "Lasagna",
-                "Bake Lasagna for 45 mins",
                 "Dinner",
                 "Italian",
+                "Bake Lasagna for 45 mins",
                 "",
                 mutableMapOf("Beef" to "6 oz", "Pasta" to "6 oz", "Sauce" to "6 oz")
             ),
             Recipe(
                 "005",
                 "Veggie Lasagna",
-                "Bake Lasagna for 45 mins",
                 "Dinner",
                 "Italian",
+                "Bake Lasagna for 45 mins",
                 "",
                 mutableMapOf("Mixed Veggies" to "6 oz", "Noodles" to "6 oz", "Sauce" to "6 oz")
             ),
             Recipe(
                 "006",
                 "Banana Bread",
-                "Put bananas in batter and bake for 30 minutes",
                 "Lunch",
                 "English",
+                "Put bananas in batter and bake for 30 minutes",
                 "",
                 mutableMapOf("Bananas" to "1 cup", "Pecans" to "1 tbsp", "flour" to "1 cup")
             ),
             Recipe(
                 "007",
                 "HongShao Pork",
-                "Wash the pork, marinate it and put it into a pot with cold water. Add seasoning and soy sauce, boil the water over high heat, and then cook over medium low heat for half an hour.",
                 "Dinner",
                 "Chinese",
+                "Wash the pork, marinate it and put it into a pot with cold water. Add seasoning and soy sauce, boil the water over high heat, and then cook over medium low heat for half an hour.",
                 "",
                 mutableMapOf(
                     "Pork" to "1 lb",
@@ -94,9 +94,9 @@ internal class RecipeServiceStub {
             Recipe(
                 "008",
                 "Fried Yum",
-                "Wash yams and carrots and slice them. Pour vegetable oil into the pot, add shallots, stir fry for a few seconds, add yam and carrot, and finally add 2g salt, 6g sugar and two tablespoons of vinegar.",
                 "Lunch",
                 "Chinese",
+                "Wash yams and carrots and slice them. Pour vegetable oil into the pot, add shallots, stir fry for a few seconds, add yam and carrot, and finally add 2g salt, 6g sugar and two tablespoons of vinegar.",
                 "",
                 mutableMapOf(
                     "Yam" to "200 g",
