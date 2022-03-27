ca668f815f4ca9a828a9710829e1ec969718a337
Zilong Chen <chen3zl@mail.uc.edu>
2022-03-27 15:09:39 -0400

    delete unused annotation

--- app/src/main/java/com/cookit/dto/MeaL.kt
+++ app/src/main/java/com/cookit/dto/MeaL.kt
@@ -1,13 +1,11 @@
 package com.cookit.dto
 
-import com.google.gson.annotations.SerializedName
-
 data class Meal(
-    @SerializedName("idRecipe")var recipeID: String,
-    @SerializedName("strRecipe")var name: String,
-    @SerializedName("idMeal")var mealId: String,
-    @SerializedName("strDescription")var description: String,
-    @SerializedName("imageURL")var image: String) {
+    var recipeID: String,
+    var name: String,
+    var mealId: String,
+    var description: String,
+    var image: String) {
     override fun toString(): String {
         return name
     }
