a40384a1850bbe56cd95e359907871bbb2d1dc63
Zilong Chen <chen3zl@mail.uc.edu>
2022-03-27 15:02:49 -0400

    add meal data class

--- app/src/main/java/com/cookit/dto/Recipe.kt
+++ app/src/main/java/com/cookit/dto/Recipe.kt
@@ -3,14 +3,25 @@ package com.cookit.dto
 import com.google.gson.annotations.SerializedName
 
 data class Recipe(
-    @SerializedName("idMeal")var recipeID : String,
-    @SerializedName("strMeal")var name : String,
+    @SerializedName("idRecipe")var recipeID : String,
+    @SerializedName("strRecipe")var name : String,
     @SerializedName("strCategory")var category : String = "",
     @SerializedName("strArea")var cuisine : String = "",
     @SerializedName("strInstructions")var instructions : String = "",
-    @SerializedName("strMealThumb")var imageURL : String = "",
+    @SerializedName("strRecipeThumb")var imageURL : String = "",
     var ingredients : MutableMap<String, String> = LinkedHashMap<String, String>()
     ) {
+    override fun toString(): String {
+        return name
+    }
+}
+
+data class Meal(
+    @SerializedName("idRecipe")var recipeID: String,
+    @SerializedName("strRecipe")var name: String,
+    @SerializedName("idMeal")var mealId: String,
+    @SerializedName("strDescription")var description: String,
+    @SerializedName("imageURL")var image: String) {
     override fun toString(): String {
         return name
     }
