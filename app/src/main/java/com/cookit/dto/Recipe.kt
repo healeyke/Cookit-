ab056d5cd7ca637a5abd0ae0cb56482bcf54cb9f
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-03-27 14:42:01 -0400

    update Recipe DTO

--- app/src/main/java/com/cookit/dto/Recipe.kt
+++ app/src/main/java/com/cookit/dto/Recipe.kt
@@ -1,12 +1,15 @@
 package com.cookit.dto
+
 import com.google.gson.annotations.SerializedName
 
 data class Recipe(
+    @SerializedName("idMeal")var recipeID : String,
     @SerializedName("strMeal")var name : String,
-    @SerializedName("instructions")var instructions : String = "",
-    @SerializedName("category")var category : String = "",
-    @SerializedName("cuisine")var cuisine : String = "",
-    @SerializedName("ingredients")var ingredients : MutableMap<String, String> = LinkedHashMap<String, String>()
+    @SerializedName("strCategory")var category : String = "",
+    @SerializedName("strArea")var cuisine : String = "",
+    @SerializedName("strInstructions")var instructions : String = "",
+    @SerializedName("strMealThumb")var imageURL : String = "",
+    var ingredients : MutableMap<String, String> = LinkedHashMap<String, String>()
     ) {
     override fun toString(): String {
         return name
