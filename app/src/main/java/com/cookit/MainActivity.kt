032eb7b08ffbbb361500399a605feed2cc4b7e13
healeyke <healeyke@mail.uc.edu>
2022-03-28 19:21:38 -0400

    added TextFieldWithDropdown amd TextFieldWithDropdownUsage

--- app/src/main/java/com/cookit/MainActivity.kt
+++ app/src/main/java/com/cookit/MainActivity.kt
@@ -4,18 +4,27 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.runtime.*
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextRange
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.PopupProperties
+import com.cookit.dto.Recipe
 import com.cookit.ui.theme.CookitTheme
 
 class MainActivity : ComponentActivity() {
+    private var inRecipeName: String = ""
+    private var selectedRecipe: Recipe? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -30,68 +39,153 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
-}
 
-@Composable
-fun RecipeFields(name: String) {
-    var recipeName by remember { mutableStateOf("")}
-    var category by remember { mutableStateOf("")}
-    var cuisine by remember { mutableStateOf("")}
-    
-    Column {
 
-        // dropdown for add vs. edit recipes
+    @Composable
+    fun RecipeFields(name: String, recipe: List<Recipe> = ArrayList<Recipe>()) {
+        //var recipeName by remember { mutableStateOf("") }
+        var category by remember { mutableStateOf("") }
+        var cuisine by remember { mutableStateOf("") }
 
-        OutlinedTextField(
+        Column {
+
+            // dropdown for add vs. edit recipes
+
+            TextFieldWithDropdownUsage(recipe)
+            /*OutlinedTextField(
             value = recipeName,
             onValueChange = {recipeName = it},
             label = { Text(stringResource(R.string.recipeName))},
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(start = 10.dp, end = 10.dp)
-        )
-        OutlinedTextField(
-            value = category,
-            onValueChange = {category = it},
-            label = { Text(text = stringResource(R.string.category))},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 10.dp, end = 10.dp)
-        )
-        OutlinedTextField(
-            value = cuisine,
-            onValueChange = {cuisine = it},
-            label = { Text(text = stringResource(R.string.cuisine))},
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(start = 10.dp, end = 10.dp)
-        )
-        Button (
-            modifier = Modifier
-                .padding(10.dp),
-            onClick = { /*TODO*/ }
-        )
-        {
-            Text(text = "Search")
+        )*/
+            OutlinedTextField(
+                value = category,
+                onValueChange = { category = it },
+                label = { Text(text = stringResource(R.string.category)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, end = 10.dp)
+            )
+            OutlinedTextField(
+                value = cuisine,
+                onValueChange = { cuisine = it },
+                label = { Text(text = stringResource(R.string.cuisine)) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp, end = 10.dp)
+            )
+            Button(
+                modifier = Modifier
+                    .padding(10.dp),
+                onClick = {
+
+                }
+            )
+            {
+                Text(text = "Search")
+            }
+
+        }
         }
 
+
+    @Composable
+    fun TextFieldWithDropdownUsage(dataIn: List<Recipe>, label: String = "", take: Int = 3) {
+
+        val dropDownOptions = remember { mutableStateOf(listOf<Recipe>()) }
+        val textFieldValue = remember { mutableStateOf(TextFieldValue()) }
+        val dropDownExpanded = remember { mutableStateOf(false) }
+
+        fun onDropdownDismissRequest() {
+            dropDownExpanded.value = false
+        }
+
+        fun onValueChanged(value: TextFieldValue) {
+            inRecipeName = value.text
+            dropDownExpanded.value = true
+            textFieldValue.value = value
+            dropDownOptions.value = dataIn.filter {
+                it.toString().startsWith(value.text) && it.toString() != value.text
+            }.take(take)
+        }
+
+        TextFieldWithDropdown(
+            modifier = Modifier.fillMaxWidth(),
+            value = textFieldValue.value,
+            setValue = ::onValueChanged,
+            onDismissRequest = ::onDropdownDismissRequest,
+            dropDownExpanded = dropDownExpanded.value,
+            list = dropDownOptions.value,
+            label = label
+        )
+    }
+
+@Composable
+fun TextFieldWithDropdown(
+    modifier: Modifier = Modifier,
+    value: TextFieldValue,
+    setValue: (TextFieldValue) -> Unit,
+    onDismissRequest: () -> Unit,
+    dropDownExpanded: Boolean,
+    list: List<Recipe>,
+    label: String = ""
+) {
+    Box(modifier) {
+        TextField(
+            modifier = Modifier
+                .fillMaxWidth()
+                .onFocusChanged { focusState ->
+                    if (!focusState.isFocused)
+                        onDismissRequest()
+                },
+            value = value,
+            onValueChange = setValue,
+            label = { Text(label) },
+            colors = TextFieldDefaults.outlinedTextFieldColors()
+        )
+        DropdownMenu(
+            expanded = dropDownExpanded,
+            properties = PopupProperties(
+                focusable = false,
+                dismissOnBackPress = true,
+                dismissOnClickOutside = true
+            ),
+            onDismissRequest = onDismissRequest
+        ) {
+            list.forEach { text ->
+                DropdownMenuItem(onClick = {
+                    setValue(
+                        TextFieldValue(
+                            text.toString(),
+                            TextRange(text.toString().length)
+                        )
+                    )
+                    selectedRecipe = text
+                }) {
+                    Text(text = text.toString())
+                }
+            }
+        }
     }
 }
 
-@Preview(name = "Light Mode", showBackground = true)
-@Preview(
-    uiMode = Configuration.UI_MODE_NIGHT_YES,
-    showBackground = true,
-    name = "Dark Mode"
-)
-@Composable
-fun DefaultPreview() {
-    CookitTheme {
-        Surface(
-            color = MaterialTheme.colors.background,
-            modifier = Modifier.fillMaxWidth()
-        ) {
-            RecipeFields("Android")
+    @Preview(name = "Light Mode", showBackground = true)
+    @Preview(
+        uiMode = Configuration.UI_MODE_NIGHT_YES,
+        showBackground = true,
+        name = "Dark Mode"
+    )
+    @Composable
+    fun DefaultPreview() {
+        CookitTheme {
+            Surface(
+                color = MaterialTheme.colors.background,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                RecipeFields("Android")
+            }
         }
     }
 }
