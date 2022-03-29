a82583b4f40b54079731c50884a6f0e37ca02ce0
ccbrowndev <46230726+ccbrowndev@users.noreply.github.com>
2022-03-29 10:48:19 -0400

    formatting and consistency for Meal dto

--- app/src/main/java/com/cookit/dto/Meal.kt
+++ app/src/main/java/com/cookit/dto/Meal.kt
@@ -3,9 +3,10 @@ package com.cookit.dto
 data class Meal(
     var recipeID: String,
     var name: String,
-    var mealId: String,
+    var mealID: String,
     var description: String,
-    var image: String) {
+    var image: String
+) {
     override fun toString(): String {
         return name
     }
