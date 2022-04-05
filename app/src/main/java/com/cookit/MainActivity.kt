b25a811e3ad0ebf7bec1083deaa8c7f0c1176e67
Kadam Patel <59839409+patel2kd@users.noreply.github.com>
2022-04-05 12:21:23 -0400

    Change3

--- app/src/main/java/com/cookit/MainActivity.kt
+++ app/src/main/java/com/cookit/MainActivity.kt
@@ -44,7 +44,7 @@ class MainActivity : ComponentActivity() {
     }
 
     @Composable
-    fun RecipeFields(name: String, recipe: List<Recipe> = ArrayList<Recipe>()) {
+    internal fun RecipeFields(name: String, recipe: List<Recipe> = ArrayList<Recipe>()) {
         var category by remember { mutableStateOf("") }
         var cuisine by remember { mutableStateOf("") }
 
