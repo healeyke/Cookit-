329c38308318ef9a3fe16d064278be290f245d20
Chris Brown <46230726+ccbrowndev@users.noreply.github.com>
2022-04-07 16:30:59 -0400

    re-add default values that were accidentally wiped out as part of the merging process

--- app/src/main/java/com/cookit/dto/Meal.kt
+++ app/src/main/java/com/cookit/dto/Meal.kt
@@ -1,10 +1,10 @@
 package com.cookit.dto
 
 data class Meal(
-    var recipeID: String,
-    var name: String,
-    var mealID: String,
-    var description: String,
+    var recipeID: String = "",
+    var name: String = "",
+    var mealID: String = "",
+    var description: String = "",
 ) {
     override fun toString(): String {
         return name
