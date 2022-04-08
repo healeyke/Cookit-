94100b274fba7237d5ed63529d753c269ac95a1e
schottlw <schottlw@mail.uc.edu>
2022-04-08 15:29:28 -0400

    Updated Meal data class to better fit data needed to fill out app fields

--- app/src/main/java/com/cookit/dto/Meal.kt
+++ app/src/main/java/com/cookit/dto/Meal.kt
@@ -2,11 +2,14 @@ package com.cookit.dto
 
 data class Meal(
     var recipeID: String = "",
-    var name: String = "",
     var mealID: String = "",
-    var description: String = "",
+    var recipeName: String = "",
+    var recipeCategory: String = "",
+    var recipeCuisine: String = "",
+    var recipeIngredients: String = "",
+    var recipeInstructions: String = "",
 ) {
     override fun toString(): String {
-        return name
+        return recipeName
     }
 }
